298d6881d86733b4a4133d631f57a2a91cedaf4f
spo587 <samueljpost@gmail.com>
2015-03-11 02:04:40 -0400

    fixed new beam bug, eighth note beams are nearly complete

--- scripts/arrayHelperFunctions.js
+++ scripts/arrayHelperFunctions.js
@@ -76,7 +76,9 @@ function arrayMax(arr){
 
 var arrayUnique = function(a) {
     return a.reduce(function(p, c) {
-        if (p.indexOf(c) < 0) p.push(c);
+        if (p.indexOf(c) < 0) {
+            p.push(c);
+        }
         return p;
     }, []);
 };
--- scripts/newBeamFunctions.js
+++ scripts/newBeamFunctions.js
@@ -6,6 +6,11 @@ function beamMeasureByBeat(voice){
         var empty = [];
         console.log(i);
         var beatNotes = beatOfNotes(empty, voice.tickables, i, 1);
+        //now deal with the stem directions bug. if the beamed notes have different stem 
+        //directions, we have to change them so they're all the same
+        if (!stemDirectionsSame(beatNotes)){
+            makeStemDirectionsSame(beatNotes)
+        }
         console.log(beatNotes);
         console.log('next line');
         if (beatNotes.length > 1){
@@ -16,6 +21,34 @@ function beamMeasureByBeat(voice){
         
     return beams;
 }
+
+function stemDirectionsSame(notes){
+    //return true if all stems are in the same direction. otherwise, return false
+    var directions = [];
+    notes.forEach(function(note){
+        directions.push(note.stem_direction);
+    });
+    return arrayUnique(directions).length === 1;
+}
+
+function makeStemDirectionsSame(notes){
+    var direction = decideStemDirection(notes);
+    //for now, let's just make it a downstem, but fix this later
+    notes.forEach(function(note){
+        note.stem_direction = direction;
+    });
+}
+
+function decideStemDirection(notes){
+    var middle = 3; //third line is middle of staff, reference for beam
+    var upOrDown = notes.reduce(function(previous, current){
+        console.log(Math.abs(current.max_line - 3));
+        return Math.abs(current.max_line - 3) > previous ? current.max_line : previous;
+    }, 0);
+    console.log(upOrDown);
+    return upOrDown > 0 ? 1 : -1;
+}
+
 
 function beatOfNotes(soFar, voiceNotes, beatStart, beatRemaining){
     if (beatRemaining === 0){
